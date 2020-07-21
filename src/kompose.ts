7a5af493f4aeb7c6ea655fac22ebb6be5aedd9ed
Ziv.Perry <ziv.perry@gmail.com>
2020-07-21 21:00:09 +0300

    use git as npm repo

--- src/kompose.ts
+++ src/kompose.ts
@@ -11,12 +11,22 @@ export function factory<T>(factoryFunction: Func): Factory<T> {
     return () => factoryFunction;
 }
 
+/**
+ * Sync version
+ * @param providers
+ * @param main
+ */
 export function kompose<T = any, R = any>(providers: Providers, main: Factory<Func<R>>) {
     return function (ctx: T): Func<R> {
         return main()(...providers().map(f => f(ctx)));
     }
 }
 
+/**
+ * Async version
+ * @param providers
+ * @param main
+ */
 export function kompoze<T = any, R = any>(providers: Providers, main: Factory<Func<R>>) {
     return function (ctx: T): Promise<Func<R>> {
         return Promise
